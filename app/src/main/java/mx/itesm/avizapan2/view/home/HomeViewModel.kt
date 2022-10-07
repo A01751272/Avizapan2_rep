f5ba2a619317f3eed58989a5a1baa9443d4c33cf
A01751272 <a01751272@itesm.mx>
2022-10-07 13:44:22 -0500

    Gradle firebase

--- app/src/main/java/mx/itesm/avizapan2/view/home/HomeViewModel.kt
+++ app/src/main/java/mx/itesm/avizapan2/view/home/HomeViewModel.kt
@@ -11,11 +11,6 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
-    }
-    val text: LiveData<String> = _text
-
     // Descargar datos del API desde una URL
     private val retrofit by lazy {
         Retrofit.Builder()
